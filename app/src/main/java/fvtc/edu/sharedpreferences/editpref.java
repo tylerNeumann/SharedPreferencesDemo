fd37c0dd2577a6db1e9877f35f92b6f133d7ccd3
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-09-23 21:21:06 -0500

    switching screens functional

--- app/src/main/java/fvtc/edu/sharedpreferences/editpref.java
+++ app/src/main/java/fvtc/edu/sharedpreferences/editpref.java
@@ -1,6 +1,10 @@
 package fvtc.edu.sharedpreferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,27 @@ public class editpref extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_editpref);
+
+        initButton();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initButton() {
+        TextView tvInfo = findViewById(R.id.tvInfo);
+        Button btnNavigate = findViewById(R.id.btnNavigate);
+
+
+        btnNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to the other screen
+                startActivity(new Intent(view.getContext(), MainActivity.class));
+            }
         });
     }
 }
